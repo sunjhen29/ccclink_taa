0f94f8c436a9b0adcc7bb4aa161bc97ac8b0be12
Sunday <sunjhen29@yahoo.com>
2018-01-20 10:58:34 +0800

    Integrating admin lte template

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,40 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+
+Route::get('admin', function () {
+    return view('layouts.admin.admin');
+});
+
+Route::get('test', function () {
+    $data['tasks'] = [
+        [
+            'name' => 'Design New Dashboard',
+            'progress' => '87',
+            'color' => 'danger'
+        ],
+        [
+            'name' => 'Create Home Page',
+            'progress' => '76',
+            'color' => 'warning'
+        ],
+        [
+            'name' => 'Some Other Task',
+            'progress' => '32',
+            'color' => 'success'
+        ],
+        [
+            'name' => 'Start Building Website',
+            'progress' => '56',
+            'color' => 'info'
+        ],
+        [
+            'name' => 'Develop an Awesome Algorithm',
+            'progress' => '10',
+            'color' => 'success'
+        ]
+    ];
+
+    return view('test')->with($data);
+});
